036a794495b288ca51c01f45ad0364d6d58805b4
Kudzai Sithole <ksithole82@gmail.com>
2022-10-11 15:55:03 +0100

    Added knight class

---
+++ Text Game/knight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventurer
+{
+    public class Knight : fighter
+    {
+        public Knight()
+        {
+            this.name = "Knight";
+            this.health = 50;
+            this.weapon = new sword();
+            this.armour = 30;
+        }
+    }
+}
