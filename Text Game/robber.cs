1f07c42c46ee63a51708a67c7f532a4f76663a86
Kudzai Sithole <ksithole82@gmail.com>
2022-10-11 14:33:08 +0100

    Added the robber class

---
+++ Text Game/robber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventurer
+{
+    public class robber : fighter
+    {
+        public robber()
+        {
+            this.name = "Robber";
+            this.health = 25;
+            this.weapon = new sword();
+            this.armour = 5;
+        }
+    }
+}
