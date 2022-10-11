928af1ead95ecbb96e61414f4ac0ef0243d1ce2b
Kudzai Sithole <ksithole82@gmail.com>
2022-10-11 16:12:40 +0100

    Used string.Equals method in main program for comparison instead of '=='

--- Text Game/Program.cs
+++ Text Game/Program.cs
@@ -47,7 +47,7 @@ namespace Adventurer
                     {
                         Console.WriteLine("Type 'a' to attack the spider");
                         string move = Console.ReadLine();
-                        if (move == "a")
+                        if (move.Equals("a"))
                         {
                             player.attack(e, player.weapon);
                             Console.WriteLine($"You have hit the spider with your {player.weapon} \n spider's health is now {e.health}.");
@@ -73,24 +73,24 @@ namespace Adventurer
                         while (true) {
                             string options = Console.ReadLine();
                             options = options.ToLower();
-                            if (options == "n")
+                            if (options.Equals("n"))
                             {
                                 Console.WriteLine($"{player.name} continues on through the forest.");
                                 break;
                             }
-                            else if (options == "y")
+                            else if (options.Equals("y"))
                             {
                                 while (true)
                                 {
                                     Console.WriteLine($"{player.name} opens the chest and finds two boxes labelled 1 and 2. Do you want to pick box 1 or box 2?");
                                     string box = Console.ReadLine();
-                                    if (box == "1")
+                                    if (box.Equals("1"))
                                     {
                                         Console.WriteLine($"{player.name} has found a chest plate! {player.name}'s armour is now {player.armour + 10}!");
                                         player.armour += 10;
                                         break;
                                     }
-                                    else if (box == "2")
+                                    else if (box.Equals("2"))
                                     {
                                         player.health -= 5;
                                         Console.WriteLine($"A baby spider jumped out of the box and bit {player.name} in the hand for 5 damage! {player.name}'s health is now {player.health} and their armour is now {player.armour}.");
@@ -123,7 +123,7 @@ namespace Adventurer
 
                         Console.WriteLine("Type 'a' to attack the robber");
                         string move = Console.ReadLine();
-                        if (move == "a")
+                        if (move.Equals("a"))
                         {
                             player.attack(enemy2, player.weapon);
                             Console.WriteLine($"You have hit the robber with your {player.weapon} \n Robber's health is now {enemy2.health} and their armour is now {enemy2.armour}.");
@@ -144,7 +144,7 @@ namespace Adventurer
                         Console.WriteLine($"{player.name} has died!");
                     }
 
-                    Console.WriteLine("By the fireplace, you find a bag full of loot! Do you want to look in the bag? \n Type 'y' to look in the bag. Type 'n' to leave the bag.");
+                    Console.WriteLine("By the fireplace, you find a mysterious bag! Do you want to look in the bag? \n Type 'y' to look in the bag. Type 'n' to leave the bag.");
                     while (true)
                     {
                         string options = Console.ReadLine();
