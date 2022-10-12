bc3467be4b320420d73ccc45ffa21c9939b474ed
Kudzai Sithole <ksithole82@gmail.com>
2022-10-12 15:50:56 +0100

    Added heal functionality to main game

--- Text Game/Program.cs
+++ Text Game/Program.cs
@@ -48,12 +48,26 @@ namespace Adventurer
                     while (e.health > 0 & player.health > 0)
                     {
                         Console.WriteLine("Type 'a' to attack the spider");
+                        Console.WriteLine("Type 'h' to heal yourself. You can only do this once per game.");
                         string move = Console.ReadLine();
                         if (move.Equals("a"))
                         {
                             player.attack(e, player.weapon);
                             Console.WriteLine($"{player.name} attacked the spider with their {player.weapon.name} \n spider's health is now {e.health}.");
                         }
+                        else if (move.Equals("h"))
+                        {
+                            if (player.hasHealed == false)
+                            {
+                                player.heal();
+                                Console.WriteLine($"{player.name} has healed themselves. Their health is now {player.health}!");
+                            }
+                            else
+                            {
+                                player.heal();
+                            }
+                        }
+
                         if (e.health > 0)
                         {
                             e.attack(player, e.weapon);
@@ -87,13 +101,13 @@ namespace Adventurer
                                 {
                                     Console.WriteLine($"{player.name} opens the chest and finds two boxes labelled 1 and 2. Do you want to pick box 1 or box 2?");
                                     string box = Console.ReadLine();
-                                    if (box.Equals("1"))
+                                    if (box.Equals("2"))
                                     {
                                         Console.WriteLine($"{player.name} has found a chest plate! {player.name}'s armour is now {player.armour + 10}!");
                                         player.armour += 10;
                                         break;
                                     }
-                                    else if (box.Equals("2"))
+                                    else if (box.Equals("1"))
                                     {
                                         player.health -= 5;
                                         Console.WriteLine($"A baby spider jumped out of the box and bit {player.name} in the hand for 5 damage! {player.name}'s health is now {player.health} and their armour is now {player.armour}.");
@@ -126,17 +140,31 @@ namespace Adventurer
                     {
 
                         Console.WriteLine("Type 'a' to attack the robber");
+                        Console.WriteLine("Type 'h' to heal yourself. You can only do this once per game.");
                         string move = Console.ReadLine();
                         if (move.Equals("a"))
                         {
                             player.attack(enemy2, player.weapon);
                             Console.WriteLine($"{player.name} attacked the robber with their {player.weapon.name} \n Robber's health is now {enemy2.health} and their armour is now {enemy2.armour}.");
                         }
+                        else if (move.Equals("h"))
+                        {
+                            if (player.hasHealed == false)
+                            {
+                                player.heal();
+                                Console.WriteLine($"{player.name} has healed themselves. Their health is now {player.health}!");
+                            }
+                            else
+                            {
+                                player.heal();
+                            }
+                        }
                         if (enemy2.health > 0)
                         {
                             enemy2.attack(player, enemy2.weapon);
                             Console.WriteLine($"Robber has attacked {player.name} with his sword! \n {player.name}'s health is now {player.health} and their armour is now {player.armour}");
                         }
+                        
 
                     }
                     if (enemy2.health <= 0)
@@ -179,6 +207,7 @@ namespace Adventurer
                     while(player.health > 0 & enemyKnight.health > 0)
                     {
                         Console.WriteLine("Type 'a' to attack the corrupt knight");
+                        Console.WriteLine("Type 'h' to heal yourself. You can only do this once in the game.");
                         string move = Console.ReadLine();
                         if (move.Equals("a"))
 
@@ -186,6 +215,19 @@ namespace Adventurer
                             player.attack(enemyKnight, player.weapon);
                             Console.WriteLine($"{player.name} attacked the corrupt knight with their {player.weapon.name} \n Knight's health is now {enemyKnight.health} and their armour is now {enemyKnight.armour}.");
                         }
+                        else if (move.Equals("h"))
+                        {
+                            if (player.hasHealed == false)
+                            {
+                                player.heal();
+                                Console.WriteLine($"{player.name} has healed themselves. Their health is now {player.health}!");
+                            }
+                            else
+                            {
+                                player.heal();
+                            }
+                        }
+
                         if (enemyKnight.health > 0)
                         {
                             enemyKnight.attack(player, enemyKnight.weapon);
