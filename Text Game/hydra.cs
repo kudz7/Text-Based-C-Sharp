e23fe43f7e52fac6faf693e868d413bc244546e1
Kudzai Sithole <ksithole82@gmail.com>
2022-10-13 10:43:30 +0100

    Added hydra enemy

---
+++ Text Game/hydra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventurer
+{
+    public class hydra : fighter
+    {
+        public hydra()
+        {
+            this.name = "Hydra";
+            this.weapon = new sword();
+            this.health = 50;
+            this.armour = 30;
+        }
+    }
+}
